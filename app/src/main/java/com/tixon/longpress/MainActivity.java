9c371e456e371f65fe23f8ac5d1a790235e1c96f
TikhonOsipov <tixon.android@yandex.ru>
2016-01-17 17:10:21 +0300

    Добавлен разбор кодов

--- app/src/main/java/com/tixon/longpress/MainActivity.java
+++ app/src/main/java/com/tixon/longpress/MainActivity.java
@@ -6,10 +6,12 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button pressArea;
+    TextView textViewLetter;
     boolean longPressed = false;
     boolean isNewMessage = true;
 
@@ -18,63 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     long criticalDot, criticalMessage;
 
-    Node root = new Node(null);
-
-    private void createRussianTree() {
-        root.setLeft(new Node("Е"));
-        root.setRight(new Node("Т"));
-        Node l1, r1, l1l2, l1r2, r1l2, r1r2;
-        Node l1l2l3, l1l2r3, l1r2l3, l1r2r3, r1l2l3, r1l2r3, r1r2l3, r1r2r3;
-        /*Node l1l2l3l4, l1l2l3r4, l1l2r3l4, l1l2r3r4, l1r2l3l4, l1r2l3r4, l1r2r3l4, l1r2r3r4,
-                r1l2l3l4, r1l2l3r4, r1l2r3l4, r1l2r3r4, r1r2l3l4, r1r2l3r4, r1r2r3l4, r1r2r3r4;*/
-        l1 = root.getLeft();
-        r1 = root.getRight();
-        l1.setLeft(new Node("И"));
-        l1.setRight(new Node("А"));
-
-        r1.setLeft(new Node("Н"));
-        r1.setRight(new Node("М"));
-
-        l1l2 = l1.getLeft();
-        l1r2 = l1.getRight();
-        r1l2 = r1.getLeft();
-        r1r2 = r1.getRight();
-
-        l1l2l3 = new Node("С");
-        l1l2r3 = new Node("У");
-        l1r2l3 = new Node("Р");
-        l1r2r3 = new Node("В");
-        r1l2l3 = new Node("Д");
-        r1l2r3 = new Node("К");
-        r1r2l3 = new Node("Г");
-        r1r2r3 = new Node("О");
-
-        l1l2.setLeft(l1l2l3);
-        l1l2.setRight(l1l2r3);
-        l1r2.setLeft(l1r2l3);
-        l1r2.setRight(l1r2r3);
-        r1l2.setLeft(r1l2l3);
-        r1l2.setRight(r1l2r3);
-        r1r2.setLeft(r1r2l3);
-        r1r2.setRight(r1r2r3);
-
-        l1l2l3.setLeft(new Node("Х"));
-        l1l2l3.setRight(new Node("Ж"));
-        l1l2r3.setLeft(new Node("Ф"));
-        l1l2r3.setRight(new Node("Ю"));
-        l1r2l3.setLeft(new Node("Л"));
-        l1r2l3.setRight(new Node("Я"));
-        l1r2r3.setLeft(new Node("П"));
-        l1r2r3.setRight(new Node("Й"));
-        r1l2l3.setLeft(new Node("Б"));
-        r1l2l3.setRight(new Node("Ь"));
-        r1l2r3.setLeft(new Node("Ц"));
-        r1l2r3.setRight(new Node("Ы"));
-        r1r2l3.setLeft(new Node("З"));
-        r1r2l3.setRight(new Node("Щ"));
-        r1r2r3.setLeft(new Node("Ч"));
-        r1r2r3.setRight(new Node("Ш"));
-    }
+    StringBuilder code = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         criticalMessage = 300;
 
         pressArea = (Button) findViewById(R.id.pressAreaCodeInterpreter);
+        textViewLetter = (TextView) findViewById(R.id.textViewLetter);
 
         pressArea.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -99,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
 
                     isNewMessage = timePauseEnd - timePauseStart >= criticalMessage;
                     Log.d("myLogs", "isNewMessage = " + isNewMessage);
+
+                    if(isNewMessage) {
+                        textViewLetter.setText(MorseParser.parse(code.toString(),
+                                getResources().getStringArray(R.array.russian_codes),
+                                getResources().getStringArray(R.array.russian_symbols)));
+                        code = new StringBuilder();
+                    }
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP) {
                     Log.d("myLogs", "up");
@@ -106,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
                     timePauseStart = timeEnd;
                     longPressed = timeEnd - timeStart >= 200;
                     Log.d("myLogs", "longPressed = " + (longPressed ? "DASH" : "DOT"));
+
+                    if(longPressed) {
+                        code.append("_");
+                    } else {
+                        code.append(".");
+                    }
                 }
                 return false;
             }
--- app/src/main/java/com/tixon/longpress/MorseParser.java
+++ app/src/main/java/com/tixon/longpress/MorseParser.java
@@ -1,13 +1,13 @@
 package com.tixon.longpress;
 
 public class MorseParser {
-    //L: 2*k + 2*(i-1)
-    //R: 2*k + 2*(i-1) + 1
-    //k - номер уровня вершины
-    //i - номер на уровне k
-    public static String[] russianAlphabet = {"Е", "Т",
-            "И", "А", "Н", "М",
-            "С", "У", "Р", "В", "Д", "К", "Г", "О",
-            "Х", "Ж", "Ф", "Ю", "Л", "Я", "П", "Й", "Б", "Ь", "Ц", "Ы", "З", "Щ", "Ч", "Ш",
-            "Э"};
+    public static String parse(String code, String[] codes, String[] symbols) {
+        String symbol = "";
+        for(int i = 0; i < codes.length; i++) {
+            if(codes[i].equals(code)) {
+                symbol = symbols[i];
+            }
+        }
+        return symbol;
+    }
 }
